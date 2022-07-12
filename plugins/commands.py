4862b664b930a95f4ee10667dca9ac4463451b98
prajith2252 <83197685+prajith2252@users.noreply.github.com>
2022-07-13 00:51:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -92,13 +92,16 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('➕ You can add out bot to your Group ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Channel', url='https://telegram.me/tmamovies')
+            InlineKeyboardButton('🤖 Channel', url='https://telegram.me/tmamovies'),
+            InlineKeyboardButton('👥 Support Chat', url='https://telegram.me/tmadiscuss')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
+        ],[
+            InlineKeyboardButton('👤 Owner', url='https://telegram.me/tmasupport')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
