5daf3194c0235904e1a4d6ed82266a32bd21482d
prajith2252 <83197685+prajith2252@users.noreply.github.com>
2022-11-27 20:49:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -10,41 +10,41 @@ def is_enabled(value, default):
         return default
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = 5540967 #int(environ['API_ID'],5540967)
-API_HASH = 'eedf0196b0533f361b51b5b7082358e9'#(environ['API_HASH'],'eedf0196b0533f361b51b5b7082358e9')
+API_ID = 9301845 #int(environ['API_ID'],5540967)
+API_HASH = '563e9fd30b529442b705c7230f766b83'#(environ['API_HASH'],'eedf0196b0533f361b51b5b7082358e9')
 BOT_TOKEN = '5003449601:AAHwaHLEmugQ1v0gzeNtAQzG75wMimScnqo' #(environ['BOT_TOKEN'],'1877486792:AAGr4aoWtD_31Qh9GGnMnV2kUNYPqSxQSkY')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/47a2366fee40ba6da37f6.jpg https://telegra.ph/file/72ca30a6a14428c36f776.jpg https://telegra.ph/file/5f091dc93a53e9a37a706.jpg https://telegra.ph/file/f5a76e1f71d5318e8035a.jpg https://telegra.ph/file/022720cc07cd137e84f59.jpg https://telegra.ph/file/06ecf5ce9045ad62f0479.jpg https://telegra.ph/file/4ac169e8871aa7be4d103.jpg https://telegra.ph/file/72d61ef5dff957ad2ff15.jpg https://telegra.ph/file/af126c7aae5e2c2a71b94.jpg https://telegra.ph/file/5425b244bc988f3534b22.jpg https://telegra.ph/file/1428e93eaca097e3046aa.jpg https://telegra.ph/file/620ebf7d4d2969837d538.jpg https://telegra.ph/file/1ea414cf5bb678ecf482b.jpg https://telegra.ph/file/8a4046ec3d2245357ecb9.jpg https://telegra.ph/file/e4d8973909f3f9beb0db5.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/b6f42af4d20b757b46d73.jpg https://telegra.ph/file/f514edecba0f6c1ef3418.jpg https://telegra.ph//file/797fcafb53479875e2bc7.jpg')).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1086048529').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', " -1001201699325 -1001511163820 -1001435020763 -1001438293617").split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', 1952992043 1713177274 5299424797 5348874903 5421706941 5673274527').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', " -1001201699325 -1001511163820 -1001435020763 -1001438293617 -1001556008450 -1001291060083 -1001465431548 -1001227500379 -1001543949874 -1001558654003 -1001438293617 -1001479888204 -1001610031839 -1001435020763 -1001636626174 -1001445318928 -1001625705075 -1001274297517 -1001374485034 -1001625705075 -1001722756764 -1001543052371 -1001651078319").split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '1086048529 1242169194 18025669102 895955663 5388296061 2117090309 1665818466 1094503092 832312885 1902119538 1478286558 5348483742 1880167885 1848232918 909693392 837573929 1267798585 1225990769 1779025877 5213335284 5148112772').split()]
 
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL','-1001367399738')
+auth_channel = environ.get('AUTH_CHANNEL','-1001860674667')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
-AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "-1001184830447 -1001367399738").split()]
+AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "-1001601961451").split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://erichdaniken:erichdaniken@cluster0.oh4dv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Dr_Strange_DataBase")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Doctor_Strange')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001112983591'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TeleXMovies')
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://tmaadda:tmaadda@cluster0.8vqs89z.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Tmamoviesbottt")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001694472922'))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Tmadiscuss')
 
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", None)
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "<b>{file_caption} \n Size :- <i>{file_size}</i> \n Join [TMAAdda](https://telegram.me/tmaadda)</b> ")
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", None)
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "<b>{file_caption} \n Size :- <i>{file_size}</i> \n Join [TMAAdda](https://telegram.me/tmaadda)</b> ")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>Query: {query}</b> \n‌‌‌‌IMDb Data:\n\n🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a>/10 **\n\n Powered By @TeleXMovies**")
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "Hey {message.from_user.mention}, \n Here is the result for your {query} \n <b>🏷 Title</b>: <a href={url}>{title}</a> \n 📆 Year: <a href={url}/releaseinfo>{year}</a> \n 🌟 Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.) \n ☀️ Languages : <code>{languages}</code> \n 📀 RunTime: {runtime} Minutes \n 📆 Release Info : {release_date} \n 🎛 Countries : <code>{countries}</code> \n \n Requested by : {message.from_user.mention} \n Powered By @TMAAdda")
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
-SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "False"), False)
+SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
 FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
